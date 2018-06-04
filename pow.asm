1b86e643b3f9a7a8f078e9085b8dfa71a1151909
GabrielRavier <gabravier@gmail.com>
2018-06-04 10:31:13 +0200

    New algorithm and minor changes
    pow.asm : Changed names for arguments (kinda dumb names before)
    
    strlen.asm : Made new implementation using repne scasb

--- pow.asm
+++ pow.asm
@@ -3,17 +3,17 @@ global @ASM_pow@8
 
 segment test
 ; According to the __fastcall calling convention, first argument goes to ecx and the second to edx
-%define number ecx  ; int
-%define power edx   ; int
+%define base ecx  ; int
+%define exponent edx   ; int
 %define result eax  ; int
 
 @ASM_pow@8:
-    mov result, 1
-    test power, power
+    mov base, 1
+    test exponent, exponent
     jle .return   ; power smaller than 1, return 1
 .loop:
-    imul result, number
-    dec power
+    imul base, base
+    dec exponent
     jnz .loop   ; while power above 0
 .return:
     ret
--- strlen.asm
+++ strlen.asm
@@ -4,17 +4,19 @@ segment text
 
 %define string ecx  ; char *, string
 %define result eax  ; length of string
+%define scasbValue al
+%define scasbAddress edi
+%define scasbLimit ecx
 
 @ASM_strlen@4:
-    xor result, result
-    cmp byte [string], 0
-    jz .returnAligned
-.loop:
-    inc result
-    cmp byte [string + result], 0
-    jnz .loop
-.return:
-    ret
-    align 16
-.returnAligned:
+    push scasbAddress   ; GCC expects us to preserve edi
+    mov scasbAddress, string
+    xor scasbLimit, scasbLimit
+    not scasbLimit  ; scasbLimit = -1
+    xor scasbValue, scasbValue  ; Search for 0
+    cld ; Clear direction flag so that repne scasb goes forward
+    repne scasb ; scasbLimit = -stringLength - 2
+    not scasbLimit ; scasbLimit = stringLength + 1
+    lea result, [scasbLimit - 1]
+    pop scasbAddress
     ret
